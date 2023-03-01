6eddb2466c8da5d3de1a116bc9f85bbe3250d18f
Genia V <evgenia.verhovtseva@yandex.ru>
2023-03-01 21:45:30 +0300

    изменение адреса

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('http://api.mesto-back.students.nomoredomains.work');
+  const api = new Api('https://api.mesto-back.students.nomoredomains.work');
 
   export default api;
