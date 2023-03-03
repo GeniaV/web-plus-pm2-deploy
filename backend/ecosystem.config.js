12587f834a84b045d40a1d0b3a9630de6889ea32
Genia V <evgenia.verhovtseva@yandex.ru>
2023-03-03 21:15:20 +0300

    исправление конфигов

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build && pm2 restart ecosystem.config.js',
+      'post-deploy': 'cd ~/web-plus-pm2-deploy/backend && npm i && npm run build && pm2 restart ecosystem.config.js',
     },
   },
 };
