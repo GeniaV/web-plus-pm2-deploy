a851de59c22b56cfc7f05bc8b6f2489e3af6bce6
Genia V <evgenia.verhovtseva@yandex.ru>
2023-03-04 17:36:29 +0300

    post-deploy change

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -19,7 +19,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'cd ~/web-plus-pm2-deploy/backend && npm i && npm run build && pm2 restart ecosystem.config.js',
+      'post-deploy': 'cd backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
