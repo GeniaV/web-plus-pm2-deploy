685c23e0b4487dd6eb909a0473628accb8c7197b
Genia V <evgenia.verhovtseva@yandex.ru>
2023-03-04 16:02:35 +0300

    change pre-deploy-local

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd ~/web-plus-pm2-deploy/backend && npm i && npm run build && pm2 restart ecosystem.config.js',
     },
   },
